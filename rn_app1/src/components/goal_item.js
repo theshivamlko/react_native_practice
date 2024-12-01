635d688525ab8f20206802becfa2c1acc2bbf432
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-12-02 01:15:50 +0530

    ripple effect

--- rn_app1/src/components/goal_item.js
+++ rn_app1/src/components/goal_item.js
@@ -10,21 +10,32 @@ import {
 
 function GoalItem(itemParam) {
   var keyExtractor = `${itemParam.itemData.item}${itemParam.itemData.index}`;
-  console.log(`FlatList Item: ${itemParam.itemData.item} ${itemParam.itemData.index}`);
+  console.log(
+    `FlatList Item: ${itemParam.itemData.item} ${itemParam.itemData.index}`
+  );
 
   return (
-    <View key={keyExtractor} style={{
+    <View
+      key={keyExtractor}
+      style={{
         flexDirection: "row",
         justifyContent: "space-between",
-    }}>
-      <Text>{itemParam.itemData.item}</Text>
-      <Pressable onPress={()=>itemParam.onDelete(itemParam.itemData.index)} > 
-        <View style={{padding:10}}>
- 
-
-        <Text>Delete</Text>
+      }}
+    >
+      <Pressable android_ripple={{color:'purple'}}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? 'yellow' : 'red',
+          padding: 10,
+        },
+      ]}
+      >
+        <Text style={{color:'#fff'}}>{itemParam.itemData.item}</Text>
+      </Pressable>
+      <Pressable onPress={() => itemParam.onDelete(itemParam.itemData.index)}>
+        <View style={{ padding: 10 }}>
+          <Text>Delete</Text>
         </View>
-
       </Pressable>
     </View>
   );
