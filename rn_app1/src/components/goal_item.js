35687851dd09cfba803e6850e5f894222331ca51
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-12-02 17:23:27 +0530

    app theme

--- rn_app1/src/components/goal_item.js
+++ rn_app1/src/components/goal_item.js
@@ -1,44 +1,37 @@
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Pressable,
-  FlatList,
-} from "react-native";
+import {Pressable, Text, View,} from "react-native";
+import Colors from "../utils/app_theme";
 
 function GoalItem(itemParam) {
-  var keyExtractor = `${itemParam.itemData.item}${itemParam.itemData.index}`;
-  console.log(
-    `FlatList Item: ${itemParam.itemData.item} ${itemParam.itemData.index}`
-  );
+    var keyExtractor = `${itemParam.itemData.item}${itemParam.itemData.index}`;
+    console.log(
+        `FlatList Item: ${itemParam.itemData.item} ${itemParam.itemData.index}`
+    );
 
-  return (
-    <View
-      key={keyExtractor}
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-    >
-      <Pressable android_ripple={{color:'purple'}}
-      style={({ pressed }) => [
-        {
-          backgroundColor: pressed ? 'yellow' : 'red',
-          padding: 10,
-        },
-      ]}
-      >
-        <Text style={{color:'#fff'}}>{itemParam.itemData.item}</Text>
-      </Pressable>
-      <Pressable onPress={() => itemParam.onDelete(itemParam.itemData.index)}>
-        <View style={{ padding: 10 }}>
-          <Text>Delete</Text>
+    return (
+        <View
+            key={keyExtractor}
+            style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+            }}
+        >
+            <Pressable android_ripple={{color: Colors.itemBackgroundColor}}
+                       style={({pressed}) => [
+                           {
+                               backgroundColor: pressed ? Colors.primary600 : Colors.primary500,
+                               padding: 10,
+                           },
+                       ]}
+            >
+                <Text style={{color: '#fff'}}>{itemParam.itemData.item}</Text>
+            </Pressable>
+            <Pressable onPress={() => itemParam.onDelete(itemParam.itemData.index)}>
+                <View style={{padding: 10}}>
+                    <Text>Delete</Text>
+                </View>
+            </Pressable>
         </View>
-      </Pressable>
-    </View>
-  );
+    );
 }
 
 export default GoalItem;
---
+++ rn_app1/src/utils/app_theme.js
@@ -0,0 +1,8 @@
+const Colors = {
+    primary500: '#bf1861',
+    primary600: '#9f144f',
+    accentColor: '#fd9e2f',
+    itemBackgroundColor: '#fd9e2f',
+}
+
+export default Colors
