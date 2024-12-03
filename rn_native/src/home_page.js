8f5c55f35c1c62e7419205b9eb4dea738695bf78
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-12-03 16:38:25 +0530

    landscap keybaord

--- rn_native/src/home_page.js
+++ rn_native/src/home_page.js
@@ -1,23 +1,54 @@
 import {StatusBar} from 'expo-status-bar';
-import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+    Pressable,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    useWindowDimensions,
+    TextInput,
+    KeyboardAvoidingView, ScrollView,
+} from 'react-native';
 
 export default function HomePage() {
+
+
+    const dimen = useWindowDimensions();
+
+    const deviceHeight = dimen.height;
+    console.log(`deviceHeight ${deviceHeight}`);
     return (
-        <View style={styles.container}>
-            <StatusBar style="dark"/>
-            <Text style={{fontSize: 20}}>Open up App.js to start working on your app!</Text>
-            <Text>Dimensions</Text>
+        <ScrollView style={{
+            flex: 1
+        }}>
+        <KeyboardAvoidingView style={{
+            flex: 1
+        }} behavior={"position"}>
 
-            <View style={{flexDirection: 'row'}}>
-                <View style={{backgroundColor: 'red', flex: 1}}/>
-                <View style={{backgroundColor: 'brown', flex: 1}}/>
-                <View style={{flexDirection: 'row', flex: 0}}>
-                    <View style={{backgroundColor: 'blue', width: 100, height: 100}}/>
-                    <View style={{backgroundColor: 'yellow', width: 20, height: 100}}/>
+
+                <View style={styles.container} >
+                    <StatusBar style="dark"/>
+                    <Text style={{fontSize: 20}}>Open up App.js to start working on your app!</Text>
+                    <Text>Dimensions</Text>
+
+                    <View style={{flexDirection: 'row'}}>
+                        <View style={{backgroundColor: 'red', flex: 1}}/>
+                        <View style={{backgroundColor: 'brown', flex: 1}}/>
+                        <View style={{flexDirection: 'row'}}>
+                            <View style={{backgroundColor: 'blue', width: 100, height: deviceHeight * 0.10}}/>
+                            <View style={{backgroundColor: 'yellow', width: 20, height: deviceHeight * 0.14}}/>
+                        </View>
+                    </View>
+
+                    <View style={{height: 40, width: '100%', borderColor: '#000', borderWidth: 1}}>
+
+                        <TextInput inputMode={'text'} disableFullscreenUI={true} />
+                    </View>
+
                 </View>
-            </View>
-
-        </View>
+        </KeyboardAvoidingView>
+        </ScrollView>
     );
 }
 
