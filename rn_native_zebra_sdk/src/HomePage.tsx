342c0006a9a6deec3a481d7bc94b48199d2764aa
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-12-05 10:02:12 +0530

    btn made

---
+++ rn_native_zebra_sdk/src/HomePage.tsx
@@ -0,0 +1,23 @@
+// add a home page component
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {Pressable, Text, View} from 'react-native';
+
+
+type SectionProps = PropsWithChildren<{
+  text: string;
+  onClick: () => void;
+}>;
+
+export default function MyButton({
+  text,
+  onClick,
+}: SectionProps): React.JSX.Element {
+  return (
+    <Pressable onPress={onClick} style={{padding: 16,backgroundColor: 'grey',margin:8}}>
+      <View >
+        <Text style={{textAlign:'center',color:'white'}}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
