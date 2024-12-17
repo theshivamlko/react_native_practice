faa8daddab898a2b783df59e5c05a7b2d7b29c5c
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-12-17 22:59:50 +0530

    drawer showing

--- rn_navigation/App.tsx
+++ rn_navigation/App.tsx
@@ -8,8 +8,8 @@ import CategoriesScreen from './src/screens/categories_screen.tsx';
 import CategoriesDetailScreen from './src/screens/categories_details.tsx';
 import * as React from 'react';
 
-// const Drawer = createDrawerNavigator();
-/*function App() {
+const Drawer = createDrawerNavigator();
+function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Welcome">
@@ -18,42 +18,42 @@ import * as React from 'react';
       </Drawer.Navigator>
     </NavigationContainer>
   );
-}*/
-
-const Stack = createNativeStackNavigator();
-
-function RootStack() {
-  return (
-    <Stack.Navigator initialRouteName={'Home'}>
-      <Stack.Screen
-        name="Home"
-        component={HomePage}
-        // options={(router,navigation)=>{}}
-        options={{
-          title: 'Home',
-          headerStyle: {
-            backgroundColor: 'pink',
-          },
-          contentStyle: {
-            backgroundColor: 'white',
-          },
-        }}
-      />
-      <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
-      <Stack.Screen
-        name="CategoriesDetailScreen"
-        component={CategoriesDetailScreen}
-      />
-    </Stack.Navigator>
-  );
 }
 
-function App() {
-  return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
-  );
-}
+// const Stack = createNativeStackNavigator();
+//
+// function RootStack() {
+//   return (
+//     <Stack.Navigator initialRouteName={'Home'}>
+//       <Stack.Screen
+//         name="Home"
+//         component={HomePage}
+//         // options={(router,navigation)=>{}}
+//         options={{
+//           title: 'Home',
+//           headerStyle: {
+//             backgroundColor: 'pink',
+//           },
+//           contentStyle: {
+//             backgroundColor: 'white',
+//           },
+//         }}
+//       />
+//       <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
+//       <Stack.Screen
+//         name="CategoriesDetailScreen"
+//         component={CategoriesDetailScreen}
+//       />
+//     </Stack.Navigator>
+//   );
+// }
+//
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <RootStack />
+//     </NavigationContainer>
+//   );
+// }
 
 export default App;
