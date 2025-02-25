96f2aa2296734c0806ae04385fff426ad652beed
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-02-25 12:18:37 +0530

    save

--- src/platform/platform_component.android.tsx
+++
@@ -1,9 +0,0 @@
-import {Text} from "react-native";
-
-
-export default function PlatformComponent() {
-
-    return (
-      <Text>ANDROID Code</Text>
-    );
-}
--- src/platform/platform_component.ios.tsx
+++
@@ -1,9 +0,0 @@
-import {Text} from "react-native";
-
-
-export default function PlatformComponent() {
-
-    return (
-      <Text>IOS Code</Text>
-    );
-}
